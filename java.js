b1a778da84e4ce7f8f5f8d63dfc0eaf392fa9faa
Avzbek <nargiza.bazarova87@gmail.com>
2024-01-21 03:24:14 -0800

    portfoilo

---
+++ java.js
@@ -0,0 +1,8 @@
+let openHam = document.querySelector('#openHam');
+let closeHam = document.querySelector('#closeHam');
+let navigationItems = document.querySelector('#navigation-items');
+const hamburgerEvent = (navigation, close, open) => {
+    navigationItems.style.display = navigation;
+    closeHam.style.display = close;
+    openHam.style.display = open;
+};
